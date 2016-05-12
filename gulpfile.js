245e1c4d35779f73141f15c8b1356e014e205db8
Tsuki <pinkyleung530@gmail.com>
2016-05-12 20:48:11 +0800

    dist data and update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
-
 // Grab node packages
-var gulp = require('gulp'),
-    rename = require('gulp-rename')
-    uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+// var merge = require('merge-stream');
+uglify = require('gulp-uglify');
 
- 
-gulp.task('compress', function() {
-  gulp.src('js/*.js')
-    .pipe(uglify())
-    .pipe(rename('typed.min.js'))
-    .pipe(gulp.dest('dist'))
+
+gulp.task('compress', function () {
+	var js = gulp.src('js/*.js')
+		.pipe(uglify())
+		.pipe(rename('typed.min.js'))
+		.pipe(gulp.dest('dist'));
+	// var data = gulp.src('cangjie.json')
+	// 	.pipe(gulp.dest('dist'));
+	// return merge(js, data);
 });
-
+gulp.task('misc', function () {
+	return gulp.src('cangjie.json')
+		.pipe(gulp.dest('dist/'));
+});
 // Default Task
 gulp.task('default', ['compress']);
+gulp.task('dist', ['compress', 'misc']);
